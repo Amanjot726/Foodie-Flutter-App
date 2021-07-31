069a6913477e1083c5750022c92b1ce0dbf7dab7
Amanjot726 <67585828+Amanjot726@users.noreply.github.com>
2021-07-31 13:20:40 +0530

    Practice Tasks

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,18 +1,58 @@
 import 'package:flutter/material.dart';
 
+final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light().copyWith(scaffoldBackgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My App"),
+          backgroundColor: Colors.green,
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (context)=>[
+                PopupMenuItem(
+                  child: Text("Item 1"),
+                  value: 1,
+                ),
+                PopupMenuItem(
+                  child: Text("Item 2"),
+                  value: 2,
+                ),
+              ],
+            ),
+          ],
+        ),
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
+    return Container(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Column(children:[Text("Hi")]),
+          Column(children:[Text("Hi")]),
           Text("Welcome"),
-          SizedBox(height: 100,),
-          Divider(height: 20.4,),
+          SizedBox(height: 20,),
+          Divider(height: 20.4),
           Text("By Amanjot"),
           Text("version : 2.4")
         ],
--- lib/Quotes.dart
+++ lib/Quotes.dart
@@ -2,6 +2,40 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light().copyWith(scaffoldBackgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My App"),
+          backgroundColor: Colors.green,
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (context)=>[
+                PopupMenuItem(
+                  child: Text("Item 1"),
+                  value: 1,
+                ),
+                PopupMenuItem(
+                  child: Text("Item 2"),
+                  value: 2,
+                ),
+              ],
+            ),
+          ],
+        ),
+        body: QuotesPage(),
+      ),
+    );
+  }
+}
 
 
 class QuotesPage extends StatefulWidget {
